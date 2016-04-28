17e52243104d9f43c965834a1c08484795d33aef
jpiquet <jRP3570@>
2016-04-28 12:39:02 +0200

    fusion login / graph

--- src/ui.js
+++ src/ui.js
@@ -1,84 +1,99 @@
 ;(function ($, undefined) {
 
-    // Code goes here...
     var ui = new artoo.ui({
-        stylesheets: ['bookmark.css', 'materialize.css']
-        //stylesheets: ['bookmark.css']
+        stylesheets: ['login.css','bookmark.css', 'materialize.css']
     });
 
-    ui.$().append(artoo.templates['bookmark.tpl']);
-
-    var loader = ui.$("#loader");
-    var scheduler = new Scheduler(loader);
-
-    atWidget.initialize(ui, $);
-    atWidget.onClose(function () {
-        scheduler.stop();
-    });
-    atWidget.onChangePeriod(function (isHour) {
-        scheduler.stop();
-        startLoading(!isHour);
-    });
-
-    var scrapper = new TagScrapper();
-    var queryGen = new QueryGenerator();
     var apiCaller = new ApiCaller($);
-    var drawer, gIsMinute, gScrapperParams;
 
-    var startLoading = function (isMinute) {
-        gIsMinute = isMinute;
-        var scrapperParams = scrapper.getParamsFromTag();
-        gScrapperParams = scrapperParams;
-        createChips(scrapperParams);
-        var finalQuery = queryGen.getQuery(scrapperParams, isMinute);
-        var graphContainer = ui.$("#graph-container");
+    var authentication = new Authentication(apiCaller);
 
-        if (drawer) {
-            drawer.clear();
-        }
-        drawer = new ChartDrawer($, graphContainer);
-        launchLoad(finalQuery);
-    };
+    atLogin.initialize(ui, $, authentication,
+        function (token) {
 
-    var launchLoad = function (query) {
-        scheduler.start(function () {
-            apiCaller.call(query, {"Authorization": "Token ZEtteHhPeW1TQWNTQU5aWnRxRi9jWEFuZ1MweGVmYWxqZHN3dU5wTVhXU2cvNjJyNjFwcElBQi8vWHBUY1VwVQ=="}, function (res) {
-                drawer.draw(res);
-                scheduler.restart();
-            }, function (err) {
+            atLogin.hide();
+
+            ui.$().append(artoo.templates['bookmark.tpl']);
+
+            var loader = ui.$("#loader");
+            var scheduler = new Scheduler(loader);
+
+            atWidget.initialize(ui, $);
+            atWidget.onClose(function () {
                 scheduler.stop();
-                console.log(err);
             });
-        }, 5000);
-    };
+            atWidget.onChangePeriod(function (isHour) {
+                scheduler.stop();
+                startLoading(!isHour);
+            });
 
-    var createChips = function (scrapperParams) {
-        atWidget.clearChips();
-        for (var param in scrapperParams) {
-            if (scrapperParams.hasOwnProperty(param)) {
-                if (typeof scrapperParams[param] === "object") {
-                    var objParam = scrapperParams[param];
-                    for (var objKey in objParam) {
-                        if (objParam.hasOwnProperty(objKey)) {
-                            atWidget.addChips(objKey, objParam[objKey], objKey !== "site", removeFilter(param + "." + objKey));
+            var scrapper = new TagScrapper();
+            var queryGen = new QueryGenerator();
+            var apiCaller = new ApiCaller($);
+            var drawer, gIsMinute, gScrapperParams;
+
+            var startLoading = function (isMinute) {
+                gIsMinute = isMinute;
+                var scrapperParams = scrapper.getParamsFromTag();
+                gScrapperParams = scrapperParams;
+                createChips(scrapperParams);
+                var finalQuery = queryGen.getQuery(scrapperParams, isMinute);
+                var graphContainer = ui.$("#graph-container");
+
+                if (drawer) {
+                    drawer.clear();
+                }
+                drawer = new ChartDrawer($, graphContainer);
+                launchLoad(finalQuery);
+            };
+
+            var launchLoad = function (query) {
+                scheduler.start(function () {
+                    apiCaller.call(query, {"Authorization": "Token "+token}, function (res) {
+                        drawer.draw(res);
+                        scheduler.restart();
+                    }, function (err) {
+                        scheduler.stop();
+                        console.log(err);
+                    });
+                }, 5000);
+            };
+
+            var createChips = function (scrapperParams) {
+                atWidget.clearChips();
+                for (var param in scrapperParams) {
+                    if (scrapperParams.hasOwnProperty(param)) {
+                        if (typeof scrapperParams[param] === "object") {
+                            var objParam = scrapperParams[param];
+                            for (var objKey in objParam) {
+                                if (objParam.hasOwnProperty(objKey)) {
+                                    atWidget.addChips(objKey, objParam[objKey], objKey !== "site", removeFilter(param + "." + objKey));
+                                }
+                            }
+                        } else {
+                            atWidget.addChips(param, scrapperParams[param], true, removeFilter(param));
                         }
                     }
-                } else {
-                    atWidget.addChips(param, scrapperParams[param], true, removeFilter(param));
                 }
-            }
-        }
-    };
+            };
 
-    var removeFilter = function (filterKey) {
-        return function () {
-            delete gScrapperParams[filterKey];
-            scheduler.stop();
-            var finalQuery = queryGen.getQuery(gScrapperParams, gIsMinute);
-            launchLoad(finalQuery);
-        };
-    };
-    
-    startLoading(true);
+            var removeFilter = function (filterKey) {
+                return function () {
+                    delete gScrapperParams[filterKey];
+                    scheduler.stop();
+                    var finalQuery = queryGen.getQuery(gScrapperParams, gIsMinute);
+                    launchLoad(finalQuery);
+                };
+            };
+
+            startLoading(true);
+        },
+        function (err) {
+            console.log(err);
+            alert(JSON.parse(err.responseText).ErrorMessage);
+            atLogin.hide();
+        });
+
+
 
 }).call(this, artoo.$);
