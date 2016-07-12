911b9c2759568b58560bce34932be3ab62c8c807
jpiquet <jRP3570@>
2016-07-12 19:08:21 +0200

    ajout PieDrawer

---
+++ src/services/PieDrawer.js
@@ -0,0 +1,58 @@
+var PieDrawer = function ($, container) {
+
+    var renderedCanvas = null;
+
+    var translateData = function (api_data) {
+
+        var labels = [], datas = [], backgroundColors=[];
+        var baseData = api_data.DataFeed[0];
+
+        for (var i = 0; i < baseData.Rows.length; i++) {
+            var row = baseData.Rows[i];
+            labels.push(row[Object.keys(row)[0]]);
+            datas.push(row[Object.keys(row)[1]]);
+            backgroundColors.push(getRandomColor())
+        }
+
+        return {
+            "labels": labels,
+            "datas": datas,
+            "backgroundColors" : backgroundColors
+        };
+    };
+
+    var getRandomColor = function () {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    var draw = function (data) {
+
+        var translatedData = translateData(data);
+
+        renderedCanvas = new Chart(container, {
+            type: 'doughnut',
+            data: {
+                labels: translatedData.labels,
+                datasets: [{
+                    data: translatedData.datas,
+                    backgroundColor: translatedData.backgroundColors
+                }]
+            }
+        });
+
+    };
+
+    var clear = function () {
+        renderedCanvas.destroy();
+    };
+
+    return {
+        "draw": draw,
+        "clear": clear
+    };
+};
