2add7772dcbf28507fda9249812c2b8bd73c8da1
jpiquet <jRP3570@>
2016-08-12 11:19:06 +0200

    Revert: suppression code inutile (4d712ce7a76b5168ae40ac18db2f66cc885c3c23)

---
+++ src/services/tagScrapper.js
@@ -0,0 +1,33 @@
+var TagScrapper = function () {
+    var getParamsFromTag = function () {
+        var params = {
+            "site": getSite()
+        };
+        return params;
+    };
+
+    var getSite = function () {
+        if (window.xtsite) {
+            return window.xtsite;
+        }
+        return getValueFromConfigInNewTag("site");
+    };
+
+    var getValueFromConfigInNewTag = function (key) {
+        if (window.ATInternet && window.ATInternet.Tracker) {
+            if (window.ATInternet.Tracker.instances && window.ATInternet.Tracker.instances[0]) {
+                return getLastIndexOfArray(window.ATInternet.Tracker.instances).getConfig(key);
+            } else if (window.ATInternet.Tracker.Tag && window.ATInternet.Tracker.Tag.prototype.instances[0]) {
+                return getLastIndexOfArray(window.ATInternet.Tracker.Tag.prototype.instances).getConfig(key);
+            }
+        }
+        return null;
+    };
+    
+    var getLastIndexOfArray = function (array) {
+        return array[array.length - 1];
+    };
+    return {
+        "getParamsFromTag": getParamsFromTag
+    };
+};
