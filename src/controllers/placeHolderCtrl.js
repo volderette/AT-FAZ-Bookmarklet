28773ae65bd3e0445deab8877c7610837c77aa51
jpiquet <jRP3570@>
2016-08-23 17:44:40 +0200

    nettoyage

--- src/controllers/placeHolderCtrl.js
+++ src/controllers/placeHolderCtrl.js
@@ -2,71 +2,25 @@ var PlaceHolderCtrl = function (options) {
 
     var drawer = options.drawer;
     var scrapperParams=options.scrapperParams;
-    var fazScrappedValues=options.fazScrappedValues;
-    var customQueryValues=options.customQueryValues;
 
     var queryGen = new QueryGenerator(options.baseQuery, customQueryValues);
     var apiCaller = new ApiCaller($);
 
     var startLoading = function () {
 
-        var finalQuery = queryGen.getQuery(scrapperParams, fazScrappedValues);
-        var finalQueryForceRT = "";
+        var finalQuery = queryGen.getQuery(scrapperParams, "");
 
-        if(options.onlyRealTime && !fazScrappedValues.period.R) {
-            //only RT and not already a RT request
-            delete fazScrappedValues.period;
-            fazScrappedValues.period = "{R:{D:0}}";
-            finalQuery = queryGen.getQuery(scrapperParams, fazScrappedValues);
-        }
-        else {
-            if (!fazScrappedValues.period.R) {
-                //add query for real time
-                delete fazScrappedValues.period;
-                fazScrappedValues.period = "{R:{D:0}}";
-                finalQueryForceRT = queryGen.getQuery(scrapperParams, fazScrappedValues);
-                if (options.evolution) {
-                    finalQueryForceRT = finalQueryForceRT + "&evo={D}";
-                    finalQuery = finalQuery + "&evo={D}";
-                }
-            } else {
-                if (options.evolution) {
-                    finalQuery = finalQuery + "&evo={H}";
-                }
-            }
+        if (options.evolution) {
+            finalQuery = finalQuery + "&evo={H}";
         }
 
         if (drawer) {
             drawer.clear();
         }
 
-        if (finalQueryForceRT == "") {
-            launchLoad(finalQuery);
-        }
-        else {
-            launchSequential(finalQuery, finalQueryForceRT);
-        }
-
+        launchLoad(finalQuery);
 
     };
-
-    var launchSequential = function (query1, query2) {
-
-        drawer.showWait();
-
-        apiCaller.callSequential(query1, query2, {"Authorization": "Token " + options.token}, function (res1, res2) {
-
-            drawer.mergeAndDraw(res1, res2, options.container);
-
-            drawer.hideWait();
-
-        }, function (err) {
-            console.log(err);
-            Notif.error(JSON.parse(err.responseText).ErrorMessage);
-            drawer.hideWait();
-        });
-    };
-
 
     var launchLoad = function (query) {
 
@@ -76,7 +30,7 @@ var PlaceHolderCtrl = function (options) {
 
             drawer.draw(res, options.container);
 
-            drawer.hideWait();//loadingElement.hide();
+            drawer.hideWait();
 
         }, function (err) {
             console.log(err);
